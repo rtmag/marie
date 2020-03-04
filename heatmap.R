6f5617b65ff20fc354f9557ef02f5905d2b4a9b8
Roberto Tirado Magallanes <rtmag4@gmail.com>
2020-03-04 14:29:17 +0800

    Create heatmap.R

---
+++ heatmap.R
@@ -0,0 +1,9 @@
+
+library(ComplexHeatmap)
+options(scipen=999)
+library(graphics)
+library(gplots)
+library(factoextra)
+library(RColorBrewer)
+colors <- rev(colorRampPalette( (brewer.pal(9, "RdBu")) )(9))
+options(bitmapType="cairo")
